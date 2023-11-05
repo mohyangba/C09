ee3ba8b8f2c18173eab62de6372531a9f6d7a5b7
mohyangba <mohyangba@users.noreply.github.com>
2023-11-05 23:56:33 +0900

    03 (1)

--- main.c
+++ main.c
@@ -5,19 +5,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int grade[N_STUDENT];
-	int i, avg, sum = 0;
-
-	 printf("input %d grades : ", N_STUDENT);
-	 
-	 for(i=0;i<N_STUDENT;i++){
-
-	 scanf("%d", &grade[i]);
-	 sum+=grade[i];
-	}
 	
-	avg = sum / N_STUDENT;
-	printf("average : %d \n", avg);
+    int grade[N_STUDENT];
+	
+	int score[N_STUDENT];
+	int i;
+	    
+      for(i=0;i<N_STUDENT;i++)
+      grade[i] = rand() % 100 + 1;
+      
+      for(i=0;i<N_STUDENT;i++)
+      score[i] = grade[i];
+      
+      for(i=0;i<N_STUDENT;i++){
+                               printf("grade[%i] = %i\t", i, grade[i]);
+                               printf("score[%i] = %i\n", i, score[i]);
+	                           }
+
 	system("PAUSE");
 	return 0;
 }
